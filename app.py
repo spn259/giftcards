76412198980f5c36041c19df811ddf0d07ba098f
Steven Nichols <stevennichols@C02G20SNML85.local>
2024-10-15 13:22:42 -0600

    colors

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def process_card(card_id):
     print("Scanning.")
     return render_template("cards.html", balance=cur_bal)
 
-local = False
+local = True
 if local:
     app.run(debug=True, host="0.0.0.0", port=8080, threaded=True, use_reloader=True)
 
