7989f7d4d682cb2435ca587c8b3c153aff5de793
Steven Nichols <stevennichols@C02G20SNML85.local>
2024-10-15 08:18:07 -0600

    new

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 
 
-from flask import Flask
+from flask import (Flask, json, jsonify, redirect, render_template, request,
+                   session, url_for, render_template_string, current_app)
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello Sammy!'
+def landing():
+    return render_template("base.html")
+
+local = False
+if local:
+    app.run(debug=True, host="0.0.0.0", port=8080, threaded=True, use_reloader=True)
 
